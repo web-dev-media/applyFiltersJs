072e92d52ebc44b828e915909956a1e6690ae0bb
Rene Reimann <rene@die-pixler.de>
2019-06-16 10:46:57 +0200

    update readme, add tests

--- README.md
+++ README.md
@@ -2,11 +2,58 @@
 
 > a simple lightweight javascript module to implement hooks in your project.
 
-See our website 
+Implemnt promise callback hooks in your projects.
 ## Install
 
 Using npm:
 
 ```sh
 npm install --save-dev applyfilters
+```
+
+## Usage
+Define a dofilter
+
+```js
+applyFilters.doFilter( 'myFilterPoint', {} ).then((filteredResult) => {});
+```
+
+use a filter
+```js
+applyFilters.addFilter('moreThenOneFilter', 'filterTwo', (resolve, filteredResult) => {
+	resolve(filteredResult);
+}, 1);
+```
+
+## Example
+
+```js
+let sayHello = () => {
+	let helloStr = '';
+
+	/*
+	 * @param {string} filterName
+	 * @param filterObj
+	 * @param args
+	 */
+	applyFilters.doFilter( 'beforeSayHello', helloStr ).then((helloStr) => {
+		alert(helloStr);
+	});
+};
+```
+filter the helloStr from other place like a new file
+
+```js
+/* 
+ * @param {string} filterName
+ * @param {string} hookName
+ * @param {function} callback
+ * @param {number} priority
+ */
+applyFilters.addFilter('beforeSayHello', 'addMyName', (resolve, helloStr) => {
+	helloStr = 'Rene';
+	resolve(helloStr);
+}, 1);
+
+sayHello();
 ```
---
+++ tests/applyFilters.test.js
@@ -0,0 +1,81 @@
+const applyFilters = require('../applyFilters').applyFilters;
+
+test('Test not used doFilter - emotyDoFilterTest', () => {
+	let testObj = 42;
+
+	applyFilters.doFilter( 'emotyDoFilterTest', testObj ).then((testObj) => {
+		expect(testObj).toBe(42);
+	});
+});
+
+test('Test Filter with exiting hookName - addClassFilterResolved', () => {
+	let testObj = {
+		something: 1,
+		somethingElse: 1,
+	};
+
+	applyFilters.doFilter( 'applyFiltersTest', testObj ).then((testObj) => {
+		expect(testObj.awesome).toBe(33);
+	});
+});
+
+test('Test Filter with more then one filter', () => {
+	let testObj = {
+		something: 1,
+		somethingElse: 1,
+	};
+
+	applyFilters.doFilter( 'moreThenOneFilter', testObj ).then((testObj) => {
+		expect(testObj.awesome).toBe(44);
+		expect(testObj.unbelivebil).toBe(66);
+	});
+});
+
+test('Test getFilter all', () => {
+	applyFilters.doFilter( 'moreThenOneFilter', {} ).then((testObj) => {});
+
+	let allFilter = applyFilters.getFilter();
+
+	expect(typeof allFilter.applyFiltersTest).toBe('object');
+	expect(typeof allFilter.moreThenOneFilter).toBe('object');
+});
+
+test('Test getFilter with filterName', () => {
+	applyFilters.doFilter( 'moreThenOneFilter', {} ).then((testObj) => {});
+
+	let allFilter = applyFilters.getFilter('moreThenOneFilter');
+
+	expect(allFilter.length).toBe(3);
+});
+
+applyFilters.addFilter('applyFiltersTest', 'addClassFilterResolved', (resolve, testObj) => {
+	testObj.awesome = 33;
+	resolve(testObj);
+}, 2);
+
+applyFilters.addFilter('moreThenOneFilter', 'filterOne', (resolve, testObj) => {
+	testObj.awesome = 44;
+	resolve(testObj);
+}, 2);
+
+applyFilters.addFilter('moreThenOneFilter', 'filterTwo', (resolve, testObj) => {
+	testObj.unbelivebil = 66;
+	resolve(testObj);
+}, 1);
+
+test('Test helloStr', () => {
+	let sayHello = () => {
+		let helloStr = 'John';
+
+		applyFilters.doFilter( 'beforeSayHello', helloStr ).then((helloStr) => {
+			expect(helloStr).toBe('Rene');
+		});
+	};
+
+	applyFilters.addFilter('beforeSayHello', 'addMyName', (resolve, str) => {
+		str = 'Rene';
+		resolve(str);
+	}, 1);
+
+	sayHello();
+});
